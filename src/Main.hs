482fa8e54313857527e6fa8744645820bb673a4e
rcs <rolf.c.stadheim@gmail.com>
2016-10-15 13:05:10 +0200

    CmdLine as opts

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,12 @@
 
+import System.Console.CmdArgs (cmdArgs)
 import qualified NS3473Column.CmdLine as CMD 
 import qualified NS3473Column.System as S
 
 
 main :: IO ()
-main = putStrLn (show CMD.cmdLine) >> 
-        case (CMD.x CMD.cmdLine) of 
-            True -> S.calcAs CMD.cmdLine
-            False -> S.runSystem CMD.cmdLine 
+main = cmdArgs CMD.cmdLine >>= \opts -> 
+        putStrLn (show opts) >> 
+        case (CMD.x opts) of 
+            True -> S.calcAs opts --CMD.cmdLine
+            False -> S.runSystem opts --CMD.cmdLine 
